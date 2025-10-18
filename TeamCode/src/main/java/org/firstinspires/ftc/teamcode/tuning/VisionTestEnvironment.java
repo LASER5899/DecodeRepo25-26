905b9b16f1e7a5a1314b4751c3068b14afe384d5
Juliette <touju47@libertycommon.org>
2025-10-18 11:17:20 -0600

    default message

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/VisionTestEnvironment.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/VisionTestEnvironment.java
@@ -1,27 +1,35 @@
 package org.firstinspires.ftc.teamcode.tuning;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+
 import org.firstinspires.ftc.teamcode.Vision;
 import org.firstinspires.ftc.teamcode.Vision.*;
-
+@Autonomous(name="Vision Test Environment", group="Linear OpMode")
 public class VisionTestEnvironment extends LinearOpMode {
+   // public VisionTestEnvironment(HardwareMap map){
+   //     this.hardwareMap=map;
+   // }
 
     public Vision camera = new Vision();
 
 
 
 
-
     @Override
     public void runOpMode(){
-        camera.setCamera("Camera 1");
+
+        WebcamName cam1 = hardwareMap.get(WebcamName.class, "Camera1");
+        //camera.setCamera("Camera1");
         camera.setTarget(Target.blue);
+        Vision.DevModeOn();
+        waitForStart();
+        camera.aprilTagSetUp(cam1);
 
-
-        camera.aprilTagSetUp();
         while(opModeIsActive()){
-            Vision.toggleDevModeOn();
             camera.scanForTarget();
         }
 
