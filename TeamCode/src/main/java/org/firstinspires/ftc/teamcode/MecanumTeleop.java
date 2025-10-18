35d658fcdba14b201b42f1c0762e3bd63b245c64
payma18 <payma18@libertycommon.org>
2025-10-18 11:44:15 -0600

    ;◠;

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleop.java
@@ -0,0 +1,166 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name="MecanumTeleop", group="Linear OpMode")
+public class MecanumTeleop extends LinearOpMode {
+
+
+    private DcMotor leftFrontDrive;
+    private DcMotor leftBackDrive;
+    private DcMotor rightFrontDrive;
+    private DcMotor rightBackDrive;
+
+    @Override
+    public void runOpMode() {
+
+        final int CYCLE_MS = 50;
+
+        double speed = 1.0;   // used to manage half speed, defaults to full speed
+        boolean invertDir = false;  // used for inverted direction prompts
+        int invDir = 1;    // used to activate inverted direction
+        boolean keyA = false, keyB = false;    // used for toggle keys
+
+        double C_LATERAL, C_AXIAL, C_YAW;
+        boolean C_HALF_SPEED, C_INV_DIR;
+
+        // Initialize the hardware variables. Note that the strings used here must correspond
+        // to the names assigned during the robot configuration step on the DS or RC devices.
+        leftFrontDrive  = hardwareMap.get(DcMotor.class, "left_front_drive");
+        leftBackDrive   = hardwareMap.get(DcMotor.class, "left_back_drive");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
+        rightBackDrive  = hardwareMap.get(DcMotor.class, "right_back_drive");
+
+        // ########################################################################################
+        // !!!            IMPORTANT Drive Information. Test your motor directions.            !!!!!
+        // ########################################################################################
+        // Most robots need the motors on one side to be reversed to drive forward.
+        // The motor reversals shown here are for a "direct drive" robot (the wheels turn the same direction as the motor shaft)
+        // If your robot has additional gear reductions or uses a right-angled drive, it's important to ensure
+        // that your motors are turning in the correct direction.  So, start out with the reversals here, BUT
+        // when you first test your robot, push the left joystick forward and observe the direction the wheels turn.
+        // Reverse the direction (flip FORWARD <-> REVERSE ) of any wheel that runs backward
+        // Keep testing until ALL the wheels move the robot forward when you push the left joystick forward.
+        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        leftFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // Wait for the game to start (driver presses PLAY)
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            // KEYBINDS
+            /*
+             * 1) Axial:    Driving forward and backward               Left-joystick Forward/Backward
+             * 2) Lateral:  Strafing right and left                     Left-joystick Right and Left
+             * 3) Yaw:      Rotating Clockwise and counter clockwise    Right-joystick Right and Left
+             */
+            C_AXIAL               = gamepad1.left_stick_y;
+            C_LATERAL             = gamepad1.left_stick_x;
+            C_YAW                 = gamepad1.right_stick_x;
+            C_HALF_SPEED          = gamepad1.a;
+            C_INV_DIR             = gamepad1.b;
+
+            double max;
+
+            // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
+            double axial   = -C_AXIAL;  // Note: pushing stick forward gives negative value
+            double lateral = -C_LATERAL;
+            double yaw     =  C_YAW;
+
+            // Combine the joystick requests for each axis-motion to determine each wheel's power.
+            // Set up a variable for each drive wheel to save the power level for telemetry.
+            double leftFrontPower  = -axial + lateral + yaw;
+            double rightFrontPower = -axial - lateral - yaw;
+            double leftBackPower   = -axial - lateral + yaw;
+            double rightBackPower  =  axial - lateral + yaw;
+
+            // Normalize the values so no wheel power exceeds 100%
+            // This ensures that the robot maintains the desired motion.
+            max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+            max = Math.max(max, Math.abs(leftBackPower));
+            max = Math.max(max, Math.abs(rightBackPower));
+
+            if (max > 1.0) {
+                leftFrontPower  /= max;
+                rightFrontPower /= max;
+                leftBackPower   /= max;
+                rightBackPower  /= max;
+            }
+
+            // This is test code:
+            //
+            // Uncomment the following code to test your motor directions.
+            // Each button should make the corresponding motor run FORWARD.
+            //   1) First get all the motors to take to correct positions on the robot
+            //      by adjusting your Robot Configuration if necessary.
+            //   2) Then make sure they run in the correct direction by modifying the
+            //      the setDirection() calls above.
+            // Once the correct motors move in the correct direction re-comment this code.
+
+
+            leftFrontPower  = gamepad1.dpad_left ? 1.0 : 0.0;  // X gamepad
+            leftBackPower   = gamepad1.dpad_down ? 1.0 : 0.0;  // A gamepad
+            rightFrontPower = gamepad1.dpad_up ? 1.0 : 0.0;  // Y gamepad
+            rightBackPower  = gamepad1.dpad_right ? 1.0 : 0.0;  // B gamepad
+
+
+            // HALF SPEED CONTROLS
+            if (C_HALF_SPEED) {
+                if (keyA == false) {
+                    keyA = !keyA;
+                    switch ((int)(speed * 10)) {
+                        case 10 : speed = 0.5; break;
+                        case 5 : speed = 1.0; break;
+                    }
+                }
+            } else {
+                keyA = false;
+            }
+
+            // INVERTED DIRECTION CONTROLS
+            if (C_INV_DIR) {
+                if (keyB == false) {
+                    keyB = !keyB;
+                    invertDir = !invertDir;
+                    if (invertDir) {
+                        invDir = -1;
+                    } else {
+                        invDir = 1;
+                    }
+                }
+            } else {
+                keyB = false;
+            }
+
+            // Send calculated power to wheels
+            leftFrontDrive.setPower(leftFrontPower * speed * invDir);
+            rightFrontDrive.setPower(rightFrontPower * speed * invDir);
+            leftBackDrive.setPower(leftBackPower * speed * invDir);
+            rightBackDrive.setPower(rightBackPower * speed * invDir);
+
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower * speed * invDir, rightFrontPower * speed * invDir);
+            telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower * speed * invDir, rightBackPower * speed * invDir);
+            telemetry.addData("Speed", "%4.2f", speed);
+            telemetry.addData("Invert Direction", "%1b", invertDir);
+            telemetry.update();
+
+            sleep(CYCLE_MS);
+
+        }
+    }
+}
