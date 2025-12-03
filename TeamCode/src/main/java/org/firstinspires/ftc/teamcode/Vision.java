90e5158d9c26b56a061e0ffb1f9dca55e9b004d8
Juliette <touju47@libertycommon.org>
2025-12-02 18:51:41 -0700

    yeet

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision.java
@@ -187,16 +187,7 @@ public class Vision {
     }
 
     public double centerDistanceCM() {
-
-        scanForTarget();
-        double centerDistance;
-        if (range == -1.0) {
-            centerDistance = -1;
-        } else {
-            centerDistance = Math.sqrt(TAG_TO_CENTER * TAG_TO_CENTER + range * range - 2 * TAG_TO_CENTER * range * Math.cos(Math.toRadians(180 - yaw)));
-        }
-
-        return centerDistance;
+        return 0.0;
     }
 
     public double horizAngle() {
@@ -305,26 +296,26 @@ public class Vision {
     AprilTagId = -1;*/
 
 
-    Pattern gamePattern = Pattern.none;
+    String gamePattern = "none";
     int scanForPatternRun = 0;
 
-    public Vision.Pattern scanForPattern() {
+    public String scanForPattern() {
 
 
-        if (gamePattern == Pattern.none && !tagProcessor.getDetections().isEmpty()) {
+        if (gamePattern == "none" && !tagProcessor.getDetections().isEmpty()) {
             //telemetry.addLine("Not empty yo :)")
 
             tag = tagProcessor.getDetections().get(0);
             if (23 == (int) tag.id) {
-                gamePattern = Pattern.PPG;
+                gamePattern = "PPG";
                 scanForPatternRun++;
             }
             if (22 == (int) tag.id) {
-                gamePattern = Pattern.PGP;
+                gamePattern = "PGP";
                 scanForPatternRun++;
             }
             if (21 == (int) tag.id) {
-                gamePattern = Pattern.GPP;
+                gamePattern = "GPP";
                 scanForPatternRun++;
             }
 
