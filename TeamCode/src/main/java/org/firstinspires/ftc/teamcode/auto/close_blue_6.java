ecf465868364a76ea165eba27350b716580d7993
payma18 <payma18@libertycommon.org>
2026-01-26 17:31:30 -0700

    new blur autos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/close_blue_6.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/close_blue_6.java
@@ -4,18 +4,14 @@ import androidx.annotation.NonNull;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
-import com.acmerobotics.roadrunner.AngularVelConstraint;
 import com.acmerobotics.roadrunner.ParallelAction;
 import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.ProfileAccelConstraint;
 import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.TranslationalVelConstraint;
-import com.acmerobotics.roadrunner.TurnConstraints;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
@@ -28,13 +24,13 @@ import com.qualcomm.robotcore.util.Range;
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 import org.firstinspires.ftc.teamcode.shooter.ShooterControl;
 @Config
-@Autonomous(name = "far blue 9", group = "Autonomous")
+@Autonomous(name = "close blue 6", group = "Autonomous")
 //@Disabled
 //psuedocode
 /*
 
  */
-public class far_blue_9 extends LinearOpMode{
+public class close_blue_6 extends LinearOpMode{
 
     // if odometry is not properly tuned or constantly being retuned:
     // you MIGHT find it useful to change these values and use multiples of them instead of direct number
@@ -211,7 +207,7 @@ public class far_blue_9 extends LinearOpMode{
 
     public class flickServo {
         private final ElapsedTime timer = new ElapsedTime();
-        private final double move_time = 0.3;
+        private final double move_time = 0.15;
         private boolean started = false;
         private Servo flicker;
         public flickServo(HardwareMap hwMap) {
@@ -274,12 +270,12 @@ public class far_blue_9 extends LinearOpMode{
 
             @Override
             public boolean run(@NonNull TelemetryPacket packet) {
-                    if (!started){t.reset(); started=true;}
-                    double dt = t.seconds();
-                    power += dt * 100;
-                    power = Range.clip(power, 0, 0.8);
-                    shooter.setPower(power);
-                    t.reset();
+                if (!started){t.reset(); started=true;}
+                double dt = t.seconds();
+                power += dt * 100;
+                power = Range.clip(power, 0, 0.8);
+                shooter.setPower(power);
+                t.reset();
                 return power < 0.7; // true reruns action
             }
         }
@@ -303,7 +299,7 @@ public class far_blue_9 extends LinearOpMode{
             public boolean run(@NonNull TelemetryPacket packet) {
                 flywheel.setKf(0.0028);
                 flywheel.setKp(0.005);
-                flywheel.setTargetRPM(980);
+                flywheel.setTargetRPM(865);
                 flywheel.flywheelHold();
                 return true; // true reruns action
             }
@@ -335,13 +331,13 @@ public class far_blue_9 extends LinearOpMode{
         flywheel = new ShooterControl(hardwareMap);
 
         Pose2d pose0 = new Pose2d(0, 0, Math.toRadians(0));
-        Pose2d pose2 = new Pose2d(-3, 0, Math.toRadians(20));
-        Pose2d pose3 = new Pose2d(-26, -12, Math.toRadians(270));
-        Pose2d pose4 = new Pose2d(-26, -45, Math.toRadians(270));
-        Pose2d pose5 = new Pose2d(-3, 0, Math.toRadians(20));
-        Pose2d pose6 = new Pose2d(-48, -12, Math.toRadians(270));
-        Pose2d pose7 = new Pose2d(-48, -45, Math.toRadians(270));
-        Pose2d pose8 = new Pose2d(-3, 0, Math.toRadians(20));
+        Pose2d pose2 = new Pose2d(45, 20, Math.toRadians(50));
+        Pose2d pose3 = new Pose2d(49, 10, Math.toRadians(-90));
+        Pose2d pose4 = new Pose2d(49, -17, Math.toRadians(-90));
+        Pose2d pose5 = new Pose2d(45, 20, Math.toRadians(50));
+        Pose2d pose6 = new Pose2d(73, 10, Math.toRadians(-90));
+        Pose2d pose7 = new Pose2d(73, -20, Math.toRadians(-90));
+        Pose2d pose8 = new Pose2d(45, 20, Math.toRadians(50));
         MecanumDrive drive = new MecanumDrive(hardwareMap, pose0);
         outtakeMotor shooter = new outtakeMotor(hardwareMap);
         transferServo transfer = new transferServo(hardwareMap);
@@ -363,32 +359,31 @@ public class far_blue_9 extends LinearOpMode{
                 .strafeToConstantHeading(new Vector2d(-15, -15), new TranslationalVelConstraint(50));
 
         TrajectoryActionBuilder one = drive.actionBuilder(pose0)
-                .strafeToConstantHeading(new Vector2d(-3, 0))//, new TranslationalVelConstraint(50))
-                .turnTo(Math.toRadians(20));
+                .strafeToLinearHeading(new Vector2d(45, 20), Math.toRadians(50));//, new TranslationalVelConstraint(10));
 
         TrajectoryActionBuilder two = drive.actionBuilder(pose2)
-                .strafeToLinearHeading(new Vector2d(-26, -12), Math.toRadians(270));//, new TranslationalVelConstraint(10)); //counterclockwise by default
+                .strafeToLinearHeading(new Vector2d(49, 10), Math.toRadians(-90));//, new TranslationalVelConstraint(10)); //counterclockwise by default
 
         TrajectoryActionBuilder three = drive.actionBuilder(pose3)
-                .strafeToConstantHeading(new Vector2d(-26, -45), new TranslationalVelConstraint(10));
+                .strafeToConstantHeading(new Vector2d(49, -17), new TranslationalVelConstraint(15));
 
         TrajectoryActionBuilder four = drive.actionBuilder(pose4)
-                .strafeToConstantHeading(new Vector2d(-3, 0))//, new TranslationalVelConstraint(10))
-                .turnTo(Math.toRadians(20));
+                .strafeToConstantHeading(new Vector2d(45, 20))//, new TranslationalVelConstraint(10))
+                .turnTo(Math.toRadians(50));
 
         TrajectoryActionBuilder five = drive.actionBuilder(pose5)
-                .strafeToConstantHeading(new Vector2d(-48, -12))//, new TranslationalVelConstraint(10))
-                .turnTo(Math.toRadians(270));
+                .strafeToConstantHeading(new Vector2d(73, 10))//, new TranslationalVelConstraint(10))
+                .turnTo(Math.toRadians(-90));
 
         TrajectoryActionBuilder six = drive.actionBuilder(pose6)
-                .strafeToConstantHeading(new Vector2d(-48, -45), new TranslationalVelConstraint(10));
+                .strafeToConstantHeading(new Vector2d(73, -20), new TranslationalVelConstraint(15));
 
         TrajectoryActionBuilder seven = drive.actionBuilder(pose7)
-                .strafeToConstantHeading(new Vector2d(-3, 0))//, new TranslationalVelConstraint(10))
-                .turnTo(Math.toRadians(20));
+                .strafeToConstantHeading(new Vector2d(45, 20))//, new TranslationalVelConstraint(10))
+                .turnTo(Math.toRadians(50));
 
         TrajectoryActionBuilder eight = drive.actionBuilder(pose8)
-                .strafeToConstantHeading(new Vector2d(-15, -15));//, new TranslationalVelConstraint(10));
+                .strafeToConstantHeading(new Vector2d(50, 5));//, new TranslationalVelConstraint(10));
 
         // actions that need to happen on init
 
@@ -399,79 +394,79 @@ public class far_blue_9 extends LinearOpMode{
         if (isStopRequested()) return;
 
         Actions.runBlocking(
-            new SequentialAction(
-
-            shooter.fireUp(),
-            new ParallelAction(
-                shooter.hold(),
-                intake.intaking(),
                 new SequentialAction(
 
-                    one.build(),
+                        shooter.fireUp(),
+                        new ParallelAction(
+                                shooter.hold(),
+                                intake.intaking(),
+                                new SequentialAction(
 
-                    transfer.toAOut(),
-                    flicker.kick(),
-                    flicker.goBack(),
-                    transfer.toBOut(),
-                    flicker.kick(),
-                    flicker.goBack(),
-                    transfer.toCOut(),
-                    flicker.kick(),
-                    flicker.goBack(),
+                                        one.build(),
 
-                    two.build(),
+                                        transfer.toAIn(),
+                                        flicker.kick(),
+                                        flicker.goBack(),
+                                        transfer.toBIn(),
+                                        flicker.kick(),
+                                        flicker.goBack(),
+                                        transfer.toCIn(),
+                                        flicker.kick(),
+                                        flicker.goBack(),
 
-                    new ParallelAction( //TODO: the transfer timer should be longer for intaking than for outtaking
-                        three.build(),
-                        new SequentialAction(
-                            transfer.toAIn(),
-                            transfer.toBIn(),
-                            transfer.toCIn(),
-                            transfer.toNeutral()
+                                        two.build(),
+
+                                        new ParallelAction( //TODO: the transfer timer should be longer for intaking than for outtaking
+                                                three.build(),
+                                                new SequentialAction(
+                                                        transfer.toAOut(),
+                                                        transfer.toBOut(),
+                                                        transfer.toCOut(),
+                                                        transfer.toNeutral()
+                                                )
+                                        ),
+                                        four.build(),
+
+                                        transfer.toAIn(),
+                                        flicker.kick(),
+                                        flicker.goBack(),
+                                        transfer.toBIn(),
+                                        flicker.kick(),
+                                        flicker.goBack(),
+                                        transfer.toCIn(),
+                                        flicker.kick(),
+                                        flicker.goBack(),
+                                        /*
+                                        five.build(),
+
+                                        new ParallelAction( //TODO: the transfer timer should be longer for intaking than for outtaking
+                                                six.build(),
+                                                new SequentialAction(
+                                                        transfer.toAOut(),
+                                                        transfer.toBOut(),
+                                                        transfer.toCOut(),
+                                                        transfer.toNeutral()
+                                                )
+                                        ),
+                                        seven.build(),
+
+                                        transfer.toAIn(),
+                                        flicker.kick(),
+                                        flicker.goBack(),
+                                        transfer.toBIn(),
+                                        flicker.kick(),
+                                        flicker.goBack(),
+                                        transfer.toCIn(),
+                                        flicker.kick(),
+                                        flicker.goBack(),
+                                        */
+                                        eight.build(),
+
+                                        shooter.stop(),
+                                        intake.stopIntaking()
+                                )
                         )
-                    ),
-                    four.build(),
-
-                    transfer.toAOut(),
-                    flicker.kick(),
-                    flicker.goBack(),
-                    transfer.toBOut(),
-                    flicker.kick(),
-                    flicker.goBack(),
-                    transfer.toCOut(),
-                    flicker.kick(),
-                    flicker.goBack(),
-
-                    five.build(),
-
-                    new ParallelAction( //TODO: the transfer timer should be longer for intaking than for outtaking
-                        six.build(),
-                        new SequentialAction(
-                            transfer.toAIn(),
-                            transfer.toBIn(),
-                            transfer.toCIn(),
-                            transfer.toNeutral()
-                        )
-                    ),
-                    seven.build(),
-
-                    transfer.toAOut(),
-                    flicker.kick(),
-                    flicker.goBack(),
-                    transfer.toBOut(),
-                    flicker.kick(),
-                    flicker.goBack(),
-                    transfer.toCOut(),
-                    flicker.kick(),
-                    flicker.goBack(),
-
-                    eight.build(),
-
-                    shooter.stop(),
-                    intake.stopIntaking()
                 )
-            )
-            )
         );
     }
 }
